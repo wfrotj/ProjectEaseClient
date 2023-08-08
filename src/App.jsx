d50d094f70b75999c28d33f1247748b9ed9babd8
wfrotj <trebrodrigo@gmail.com>
2023-08-08 08:50:13 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useState } from "react";
+import { storage } from "./firebase";
+import { ref, uploadBytes } from "firebase/storage";
+import { v4 } from "uuid";
+
+function App() {
+  const [imageUpload, setImageUpload] = useState(null);
+  const uploadImage = () => {
+    if (imageUpload === null) return;
+    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+
+    uploadBytes(imageRef, imageUpload).then(() => {
+      alert("Image Uploaded");
+    });
+  };
+  return (
+    <div className="app">
+      <input type="file" onChange={(e) => setImageUpload(e.target.files[0])} />
+      <button onClick={uploadImage}>Upload Image</button>
+    </div>
+  );
+}
+
+export default App;
