cbd9cf56c27fdeb1268631909fa3038f9f407470
wfrotj <trebrodrigo@gmail.com>
2024-01-28 08:49:54 +0800

    edit viteconfig

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-  server: {
-    proxy: "http://localhost:3000",
+  build: {
+    outDir: "dist",
   },
+  /*   server: {
+    proxy: "http://localhost:3005",
+  }, */
 });
